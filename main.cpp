d3bb11b7176e2f99baa3d44f462a5d1b9617faaf
RealHath <1203483380@qq.com>
2021-06-10 14:21:24 +0800

    fix:接收错误的请求数据格式时，会导致崩溃

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "main.h"
 #include "sql.h"
 
+int code_convert(char *inbuf, int inlen, char *outbuf, int outlen)
+{
+    iconv_t cd;
+    int rc;
+    char **pin = &inbuf;
+    char **pout = &outbuf;
+
+    cd = iconv_open("UTF-8", "GB2312");
+    if (cd == 0)
+        return -1;
+    memset(outbuf, 0, outlen);
+    if (iconv(cd, pin, &inlen, pout, &outlen) == -1)
+        return -1;
+    iconv_close(cd);
+    return 0;
+}
+
 //解析post请求数据
 void get_post_message(std::string &buf, struct evhttp_request *req)
 {
@@ -18,75 +35,91 @@ void get_post_message(std::string &buf, struct evhttp_request *req)
         buf = std::string((const char *)evbuffer_pullup(req->input_buffer, -1));
     }
 }
-
+//new
 void login(evhttp_request *req, void *arg)
 {
-    const cJSON *name = nullptr;
-    const cJSON *pass = nullptr;
-    cJSON *monitor = cJSON_CreateObject();
     if (req == nullptr)
     {
-        std::cout << "====line:" << __LINE__ << "input param req is null." << std::endl;
+        //std::cout << "====line:" << __LINE__ << "input param req is null." << std::endl;
         return;
     }
-
-    std::string buf(BUF_MAX, 0);
+    std::string buf;
     get_post_message(buf, req); //获取请求数据，一般是json格式的数据
     if (buf.empty())
     {
-        std::cout << "====line:" << __LINE__ << "get_post_message return null." << std::endl;
+        //std::cout << "====line:" << __LINE__ << "get_post_message return null." << std::endl;
         return;
     }
+
+    //可以使用json库解析需要的数据
+
+    //响应的buffer
+    struct evbuffer *retbuff = evbuffer_new();
+    cJSON *monitor = cJSON_CreateObject();
+
+    //请求数据格式错误
+    cJSON *json = cJSON_Parse(buf.c_str());
+    if (!json)
+    {
+        cJSON_AddItemToObject(monitor, "data", nullptr);
+        cJSON_AddStringToObject(monitor, "msg", "数据格式错误");
+        cJSON_AddNullToObject(monitor, "systemCode");
+        cJSON_AddNumberToObject(monitor, "statusCode", 400);
+        std::string tmp(cJSON_Print(monitor));
+        evbuffer_add_printf(retbuff, "%s", tmp.c_str());
+        evhttp_send_reply(req, HTTP_BADREQUEST, "bad request", retbuff);
+        evbuffer_free(retbuff);
+        return;
+    }
+
+    std::vector<std::string> item = {"", "Username", "Password", "Phone", "Sex",
+                                     "Email", "Integral", "Birthday",
+                                     "Membership_code", "Create_time", "Update_time"};
+
+    cJSON *username = cJSON_GetObjectItemCaseSensitive(json, "username");
+    cJSON *password = cJSON_GetObjectItemCaseSensitive(json, "password");
+    std::string sql = "select * from user where Username = \"" + std::string(username->valuestring) +
+                      "\" and Password = \"" + std::string(password->valuestring) + "\";";
+    //std::cout << sql << std::endl;
+    MYSQL_RES *res = MyDB::getInstance()->searchValuesToRes(sql.c_str());
+    //查询结果正确
+    if (res) // 返回了结果集
+    {
+        cJSON *monitor2 = cJSON_CreateObject();
+        int num_fields = mysql_num_fields(res); //获取结果集中总共的字段数，即列数
+        //获取下一行数据
+        MYSQL_ROW row = mysql_fetch_row(res);
+        for (int j = 1; j < num_fields; j++) //输出每一字段
+        {
+            std::cout << row[j] << "   ";
+            cJSON_AddStringToObject(monitor2, item[j].c_str(), row[j]);
+        }
+        std::cout << std::endl;
+        cJSON_AddItemToObject(monitor, "data", monitor2);
+        cJSON_AddStringToObject(monitor, "msg", "login success");
+        cJSON_AddNullToObject(monitor, "systemCode");
+        cJSON_AddNumberToObject(monitor, "statusCode", 200);
+        std::string tmp(cJSON_Print(monitor));
+        evbuffer_add_printf(retbuff, "%s", tmp.c_str());
+        evhttp_send_reply(req, HTTP_OK, "", retbuff);
+        evbuffer_free(retbuff);
+        cJSON_free(monitor2);
+    }
+    //查询结果错误
     else
     {
-        //可以使用json库解析需要的数据
-
-        cJSON *json = cJSON_Parse(buf.c_str());
-        if (!json)
-        {
-            struct evbuffer *retbuff = evbuffer_new();
-            evbuffer_add_printf(retbuff, "数据格式错误!");
-            evhttp_send_reply(req, HTTP_BADREQUEST, "bad request", retbuff);
-            evbuffer_free(retbuff);
-            return;
-        }
-
-        std::vector<std::string> item = {"Username", "Password", "Phone", "Sex", "Email", "Itegral", "Birthday", "Membership_code", "Create_time", "Update_time"};
-        name = cJSON_GetObjectItemCaseSensitive(json, "username");
-        pass = cJSON_GetObjectItemCaseSensitive(json, "password");
-
-        std::string sql = "select * from user where Username = \"" + std::string(name->valuestring) +
-                          "\" and Password = \"" + std::string(pass->valuestring) + "\";";
-        std::cout << sql << std::endl;
-        MYSQL_RES *res = MyDB::getInstance()->searchValuesToRes(sql.c_str());
-        if (res) // 返回了结果集
-        {
-            cJSON *monitor2 = cJSON_CreateObject();
-            int num_fields = mysql_num_fields(res); //获取结果集中总共的字段数，即列数
-            //获取下一行数据
-            MYSQL_ROW row = mysql_fetch_row(res);
-            for (int j = 0; j < num_fields; j++) //输出每一字段
-            {
-                cJSON_AddStringToObject(monitor2, item[j].c_str(), row[j]);
-            }
-            std::cout << std::endl;
-            cJSON_AddItemToObject(monitor, "data", monitor2);
-            cJSON_AddStringToObject(monitor, "msg", "login success");
-            cJSON_AddNullToObject(monitor, "system");
-            cJSON_AddNumberToObject(monitor, "statusCode", 200);
-        }
+        cJSON_AddItemToObject(monitor, "data", nullptr);
+        cJSON_AddStringToObject(monitor, "msg", "查询结果为空");
+        cJSON_AddNullToObject(monitor, "systemCode");
+        cJSON_AddNumberToObject(monitor, "statusCode", 200);
+        std::string tmp(cJSON_Print(monitor));
+        evbuffer_add_printf(retbuff, "%s", tmp.c_str());
+        evhttp_send_reply(req, HTTP_BADREQUEST, "", retbuff);
+        evbuffer_free(retbuff);
     }
-
-    struct evbuffer *retbuff = nullptr;
-    retbuff = evbuffer_new();
-    if (!retbuff)
-    {
-        std::cerr << "====line:" << __LINE__ << "retbuff in nullptr" << std::endl;
-        return;
-    }
-    evbuffer_add_printf(retbuff, cJSON_Print(monitor));
-    evhttp_send_reply(req, HTTP_OK, "no resource", retbuff);
-    evbuffer_free(retbuff);
+    cJSON_free(monitor);
+    cJSON_free(username);
+    cJSON_free(password);
 }
 
 void page404(evhttp_request *req, void *arg)
